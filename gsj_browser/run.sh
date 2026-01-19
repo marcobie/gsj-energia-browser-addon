cb5f16be3936536666c6dc2144ed4955f8f7b553
Marcin Obierzyński <58610604+marcobie@users.noreply.github.com>
2026-01-19 17:26:28 +0100

    Update run.sh

--- gsj_browser/run.sh
+++ gsj_browser/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-echo "Starting GSJ Browser API..."
+
+export GSJ_USERNAME="${USERNAME}"
+export GSJ_PASSWORD="${PASSWORD}"
+
+echo "Starting GSJ Browser API with user: $GSJ_USERNAME"
+
 cd /app
 uvicorn server:app --host 0.0.0.0 --port 8124
